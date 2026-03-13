ff7038a7dd9466a03f699ad2c538ae1ebaa11b87
aqeelahhh <ST10516749@myemeris.edu.za>
2026-03-13 19:07:19 +0200

    Added calculations

--- app/src/main/java/com/example/contactactivity/MainActivity.kt
+++ app/src/main/java/com/example/contactactivity/MainActivity.kt
@@ -11,6 +11,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val studentAge = 21
+
+        // Years until student turns 100
+        val yearsTo100 = 100 - studentAge
+
+        // Age in months
+        val ageInMonths = studentAge * 12
+
+        // Age in days (skip leap years, use 365 days)
+        val ageInDays = studentAge * 365
+
+        // Reminder when age divided by 5
+        val remainderBy5 = studentAge * 0.5
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
